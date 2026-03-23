982b7dbf1b90a65cebf8a5a385fdbc29c1b57435
adiadnanadi <adnansahanic40@gmail.com>
2026-03-24 00:18:47 +0100

    Update app.py
    aaa

--- backend/app.py
+++ backend/app.py
@@ -75,6 +75,11 @@ def ucitaj_sve_encodinge() -> list:
             })
     return korisnici
 
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
 
 @app.route("/health", methods=["GET"])
 def health():
