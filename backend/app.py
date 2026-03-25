9b6d3c788ed1c44a923aa2f085e8054a4b038e04
adiadnanadi <adnansahanic40@gmail.com>
2026-03-25 19:04:19 +0100

    Update app.py
    nvoo

--- backend/app.py
+++ backend/app.py
@@ -46,11 +46,11 @@ def init_model():
     global face_app
     try:
         print("[STARTUP] Inicijaliziram InsightFace model...")
-        face_app = FaceAnalysis(
-    name="buffalo_l",
+       face_app = FaceAnalysis(
+    name="buffalo_sc",
     providers=["CPUExecutionProvider"]
 )
-face_app.prepare(ctx_id=0, det_size=(640, 640))
+face_app.prepare(ctx_id=0, det_size=(320, 320))
         print("[STARTUP] InsightFace model spreman!")
     except Exception as e:
         print(f"[STARTUP] Greška modela: {e}")
