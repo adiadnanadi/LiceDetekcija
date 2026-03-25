7fb682fcc972112ad912fae65e0c62aa2e7c027c
adiadnanadi <adnansahanic40@gmail.com>
2026-03-25 19:19:21 +0100

    Update app.py
    aa

--- backend/app.py
+++ backend/app.py
@@ -48,7 +48,7 @@ try:
         name="buffalo_l",
         providers=["CPUExecutionProvider"]
     )
-    face_app.prepare(ctx_id=0, det_size=(640, 640))
+    face_app.prepare(ctx_id=0, det_size=(320, 320))
     print("[STARTUP] ✅ InsightFace model spreman!")
 except Exception as e:
     print(f"[STARTUP] ❌ Greška modela: {e}")
